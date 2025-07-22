57f880724a6d0f4c318c48571b99e2fc64de4333
Ameer PB <ameerpbabu@gmail.com>
2025-07-22 12:01:18 +0530

    Add SG group.tf

---
+++ Docker/Prowler/Terraform/security_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "prowler" {
+  name        = "allow_ssh_3000"
+  description = "Allow SSH and port 3000 from my IP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
