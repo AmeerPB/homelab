bfe4b788534e9d67d44cf9a389589fbca54ae214
Ameer pb <ameerpbabu@gmail.com>
2025-07-27 14:14:54 +0530

    Add kube-server (master)

---
+++ Proxmox/Terraform/kube-server.tf
@@ -0,0 +1,40 @@
+resource "proxmox_vm_qemu" "kube-server" {
+  count = 1
+  name = "kube-server-0${count.index + 1}"
+  target_node = "prox-1u"
+  vmid = "41${count.index + 1}"
+  clone = "k8s-Master"
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 4096
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "30G"
+    type = "scsi"
+    storage = "local-lvm"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.1.10${count.index + 1}/24,gw=192.168.1.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
