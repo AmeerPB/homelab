3b11f29f06b102ed84ee520589112a17240f895e
Ameer pb <ameerpbabu@gmail.com>
2025-07-27 14:04:08 +0530

    Added readme and main.tf

---
+++ Proxmox/Terraform/main.tf
@@ -0,0 +1,6 @@
+provider "proxmox" {
+  pm_api_url = "https://russia.machinesarehere.in:8006/api2/json"
+  pm_api_token_id = var.PM_API_TOKEN_ID
+  pm_api_token_secret = var.PM_API_TOKEN_SECRET
+  pm_tls_insecure = false
+}
---
+++ Proxmox/Terraform/readme.md
@@ -0,0 +1,1 @@
+## Terraform to provision VM's
