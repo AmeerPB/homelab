84dfbdc3b3a5943a0cb6026ed157b17817729355
Ameer pb <ameerpbabu@gmail.com>
2025-07-27 14:09:54 +0530

    Add versions and proxmox provider

---
+++ Proxmox/Terraform/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.2-rc03"
+    }
+  }
+}
+
+}
